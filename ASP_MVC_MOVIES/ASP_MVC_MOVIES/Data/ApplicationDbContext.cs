6962606b264ce1b590d209f218635a78d7d14151
Leopold Juster <leojuster16@gmail.com>
2021-12-03 11:22:42 +0000

    MVC Movies

---
+++ ASP_MVC_MOVIES/ASP_MVC_MOVIES/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ASP_MVC_MOVIES.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASP_MVC_MOVIES.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
---
+++ ASP_MVC_MOVIES/ASP_MVC_MOVIES/Models/Movies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_MVC_MOVIES.Models
+{
+    public enum Genre
+    {
+        Comedy, Drama, Action, Thriller
+    }
+    public class Movies
+    {
+        [Key]
+        public int ID { get; set; }
+        [StringLength(60), Required]
+        
+        public string Title { get; set; }
+       
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public Genre Genre { get; set; } = Genre.Action;
+
+        [Range(5.99, 7.99), Required]
+        public decimal Price { get; set; }
+
+    }
+}
