48f987777e6459ef861719aead197861705aeede
Leopold Juster <leojuster16@gmail.com>
2021-12-03 12:25:16 +0000

    MVC stage 1

---
+++ ASP_RAZOR_CONTOSO/ASP_RAZOR_CONTOSO/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_RAZOR_CONTOSO.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+       
+        //Relationships or navigation properites
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
